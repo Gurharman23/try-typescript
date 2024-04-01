783102acd510565405396cca6d0f939d958d87a3
Gurharmanjit Singh <gurharmanjit.singh@jungleworks.com>
2024-04-01 23:54:43 +0530

    tsc basic setup

---
+++ built/local/tsc.js
@@ -0,0 +1,6 @@
+console.log("Hello world!");
+var someFunction = function (x, y) {
+    return "".concat(x, " + ").concat(y);
+};
+someFunction("as", "fasdf");
+//# sourceMappingURL=tsc.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log("Hello world!");
+
+const someFunction = (x: string, y: string) => {
+  return `${x} + ${y}`;
+};
+someFunction("as", "fasdf");
